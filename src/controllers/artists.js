1a73bc50e1973687e1a3809d3029512b04c80d49
Tom Haynes <tomhaynes94@gmail.com>
2022-06-08 21:14:20 +0100

    added read test, read controller and route for artists

--- src/controllers/artists.js
+++ src/controllers/artists.js
@@ -1,17 +1,17 @@
 const getDb = require('../services/db');
 
-// artist controller func returning a 201 status code
+// create an artist controller func returning a 201 status code
 exports.create = async (request, response) => {
     const db = await getDb();
     const { name, genre } = request.body;
 
     try {
         // await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
-        
-        await db.query(`INSERT INTO Artist (name, genre) VALUES ('?, ?')`, [
-            name,
-            genre,
-        ]);  // preventing SQL injection by providing '?' variables in an array
+
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre,
+    ]);  // preventing SQL injection by providing '?' parameters to form an array
     
         response.sendStatus(201);
       } catch (err) {
@@ -20,3 +20,17 @@ exports.create = async (request, response) => {
     
       db.close();
     };
+
+    exports.read = async (request, response) => {
+        const db = await getDb();
+
+    try {
+        const [artists] = await db.query('SELECT * FROM Artist');
+        console.log([artists]);
+
+    response.status(200).json(artists);
+    } catch (err) {
+    response.status(500).json(err);
+    }
+    db.close();
+  };
--- src/routes/artists.js
+++ src/routes/artists.js
@@ -7,4 +7,6 @@ const router = express.Router();
 
 router.post('/', artistController.create);
 
+router.get('/', artistController.read);
+
 module.exports = router;
